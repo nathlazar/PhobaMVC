cdbbc7f3c046d02316cc9d46195f80cf8f7cf5af
Lazar-PC <Lazar-PC@DESKTOP-6DGDDSO>
2021-06-27 20:02:32 +0800

    Added UpdateView functional interface if it is not necessary to invoke the default updateView constantly from View interface

---
+++ src/main/java/com/phobamvc/ui/UpdateView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.phobamvc.ui;
+
+/**
+ *
+ * @author Lazar-PC
+ */
+
+@FunctionalInterface
+public interface UpdateView {
+    
+    public abstract void updateView();
+    
+}
